52ef84f8d531c60bba87e8c7a7224b9f07c2538f
ZhangXinNan <zhangxin19870504@163.com>
2020-12-23 01:37:49 +0800

    table

--- tools/table/table_main.py
+++ tools/table/table_main.py
@@ -1,17 +1,32 @@
 
 import os
 import argparse
+import numpy as np
+import cv2
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', default='')
-    parser.add_argument('--outdir', default='')
+    parser.add_argument('--input', default='./data')
+    parser.add_argument('--out_dir', default='./data_result')
     return parser.parse_args()
 
 
 def main(args):
-    pass
+    if not os.path.isdir(args.out_dir):
+        os.makedirs(args.out_dir)
+
+    if os.path.isfile(args.input):
+        img = cv2.imread(args.input)
+        print(img.shape)
+    elif os.path.isdir(args.input):
+        for filename in os.listdir(args.input):
+            name, suffix = os.path.splitext(filename)
+            if suffix.lower() not in ['.jpg', '.jpeg', '.png']:
+                continue
+            img_path = os.path.join(args.input, filename)
+            img = cv2.imread(img_path)
+            print(img.shape)
 
 
 if __name__ == '__main__':
