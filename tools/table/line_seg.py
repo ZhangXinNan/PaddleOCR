bd3ac52bb8840073913f1fffe6062cd779d0dd52
zhangxin <zhangxin19870504@163.com>
2020-12-23 11:21:06 +0800

    使用opencv进行直线检测

---
+++ tools/table/line_seg.py
@@ -0,0 +1,119 @@
+
+import math
+
+
+def get_line_para(x1, y1, x2, y2):
+    A = y1 - y2
+    B = x2 - x1
+    C = x1 * y2 - x2 * y1
+    return A, B, C
+
+
+def line_length(x1, y1, x2, y2):
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+
+def get_cross_point(a1, b1, c1, a2, b2, c2):
+    x = (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1)
+    y = (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1)
+    return round(x), round(y)
+
+
+def get_x_from_line(A, B, C, y):
+    if B == 0.0:
+        return -1.0 * C / A
+    elif A == 0.0:
+        return 0.0
+    return -1.0 * (B * y + C) / A
+
+
+def get_y_from_line(A, B, C, x):
+    if A == 0.0:
+        return -1.0 * C / B
+    elif B == 0.0:
+        return 0.0
+    return -1.0 * (A * x + C) / B
+
+
+def get_line_vertical(A, B, C, pt):
+    px, py = pt[0], pt[1]
+    if A == 0:
+        return -1 * B, 0, B * px
+    elif B == 0:
+        return 0, -1 * A, A * py
+    else:
+        return -1 * B, A, B * px - A * py
+    return 0, 0, 0
+
+
+class LineSeg:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1, self.y1 = x1, y1
+        self.x2, self.y2 = x2, y2
+        self.A, self.B, self.C = get_line_para(x1, y1, x2, y2)
+        self.length = line_length(x1, y1, x2, y2)
+
+    def get_cross_point(self, line):
+        a1, b1, c1 = self.A, self.B, self.C
+        a2, b2, c2 = line.A, line.B, line.C
+        x = (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1)
+        y = (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1)
+        return round(x), round(y)
+
+    def get_cross_point_by_param(self, a2, b2, c2):
+        a1, b1, c1 = self.A, self.B, self.C
+        x = (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1)
+        y = (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1)
+        return int(round(x)), int(round(y))
+
+    def get_line_vertical(self, pt):
+        px, py = pt[0], pt[1]
+        AA, BB, CC = 0, 0, 0
+        if self.A == 0:
+            BB = 0
+            AA = -1 * self.B
+            CC = self.B * px
+        elif self.B == 0:
+            AA = 0
+            BB = -1 * self.A
+            CC = self.A * py
+        else:
+            AA, BB = -1 * self.B, self.A
+            CC = -1 * (AA * px + BB * py)
+        return AA, BB, CC
+
+    def left(self):
+        if self.x1 > self.x2:
+            return self.x2, self.y2
+        return self.x1, self.y1
+
+    def right(self):
+        if self.x1 > self.x2:
+            return self.x1, self.y1
+        return self.x2, self.y2
+
+    def top(self):
+        if self.y1 > self.y2:
+            return self.x2, self.y2
+        return self.x1, self.y1
+
+    def bottom(self):
+        if self.y1 > self.y2:
+            return self.x1, self.y1
+        return self.x2, self.y2
+
+    def get_x(self, y):
+        if self.B == 0.0:
+            return -1.0 * self.C / self.A
+        elif self.A == 0.0:
+            return 0.0
+        return -1.0 * (self.B * y + self.C) / self.A
+
+    def get_y(self, x):
+        if self.A == 0.0:
+            return -1.0 * self.C / self.B
+        elif self.B == 0.0:
+            return 0.0
+        return -1.0 * (self.A * x + self.C) / self.B
+
+
--- tools/table/table_main.py
+++ tools/table/table_main.py
@@ -1,8 +1,26 @@
 
 import os
 import argparse
-import numpy
+import numpy as np
 import cv2
+import random
+
+from line_seg import line_length
+_CHAR_SIZE = 32
+
+
+def draw_lines(img, lines_hor, lines_ver):
+    img_show = img.copy()
+    for line in lines_hor:
+        line = line[0].astype(np.int)
+        x1, y1, x2, y2 = line[0], line[1], line[2], line[3]
+        cv2.line(img_show, (x1, y1), (x2, y2), (255, 0, 0), 3)
+    for line in lines_ver:
+        line = line[0].astype(np.int)
+        x1, y1, x2, y2 = line[0], line[1], line[2], line[3]
+        cv2.line(img_show, (x1, y1), (x2, y2), (0, 255, 0), 3)
+    img_show = cv2.addWeighted(img, 0.5, img_show, 0.5, 0)
+    return img_show
 
 
 def get_args():
@@ -27,6 +45,28 @@ def main(args):
             img_path = os.path.join(args.input, filename)
             img = cv2.imread(img_path)
             print(img_path, img.shape)
+
+            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img_gaus = cv2.GaussianBlur(img_gray, (5, 5), 0)
+
+            fld = cv2.ximgproc.createFastLineDetector()
+            lines = fld.detect(img_gaus)
+            print(lines)
+            # lines_valid = []
+            lines_valid_hor, lines_valid_ver = [], []
+            for i, line in enumerate(lines):
+                line = line[0].astype(np.int)
+                x1, y1, x2, y2 = line[0], line[1], line[2], line[3]
+                # 大于文本高度1.5倍的才算有效的直线
+                # if img_mask is not None:
+                if line_length(x1, y1, x2, y2) > _CHAR_SIZE * 1.5:
+                    # lines_valid.append(lines[i])
+                    if abs(x1 - x2) > 10 * abs(y1 - y2):
+                        lines_valid_hor.append(lines[i])
+                    elif abs(y1 - y2) > 10 * abs(x1 - x2):
+                        lines_valid_ver.append(lines[i])
+            img_show_lines = draw_lines(img, lines_valid_hor, lines_valid_ver)
+            cv2.imwrite(os.path.join(args.out_dir, filename + '.lines.jpg'), img_show_lines)
 
 
 if __name__ == '__main__':
